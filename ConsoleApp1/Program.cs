0bb081bd34305e9e835d00b73ebd68d579c00b31
Hayat Jahangir Khan <112794077+HAYATJAHANGIRKHAN@users.noreply.github.com>
2024-11-18 08:37:47 +0530

    Add files via upload
    aboyt c#  data types and typeCasting

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a=20, b=30;
+            // int c = a + b;
+            // Console.WriteLine(c);
+            // string inp= Console.ReadLine();
+            //int a = 50;
+            //float b = 43.5f;
+            //double c = 99.3d;
+            //Console.WriteLine(a+b);
+
+            // TypeCasting 
+            // implicit TypeCasting (convert small to Large)
+            // char to int to long to float to double
+            int x = (int)3.5;
+            Console.WriteLine(x);
+            Console.ReadLine();
+
+
+        }
+    }
+}
